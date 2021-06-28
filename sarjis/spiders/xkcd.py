288fb6eb03831f28769ac84b2499a955ed2259c8
Jukka <jukkamic@gmail.com>
2021-06-28 02:22:09 +0000

    getting started with xkcd spider

---
+++ sarjis/spiders/xkcd.py
@@ -0,0 +1,39 @@
+import scrapy
+
+
+class XkcdSpider(scrapy.Spider):
+    name = 'xkcd'
+ #   allowed_domains = ['xkcd.com']
+ #   start_urls = ['http://xkcd.com/']
+#    allowed_domains = ['*']
+    start_urls = ["http://54.217.147.107/xkcd/"]
+
+    def parse(self, response):
+            for comic in response.xpath('//div[@id="middleContainer"]'):
+#            comic = response.xpath('//div[@id="middleContainer"]')
+
+#            print("COMIC === " + comic)
+                title = comic.xpath('//div[@id="ctitle"]/text()').extract_first()
+                imgurl = comic.xpath('//div[@id="comic"]/img/@src').extract_first()
+                alt = comic.xpath('//div[@id="comic"]/img/@title').extract_first()
+#            alt = response.xpath(('//div[@id="comic"]/img/@title').extract_first()
+
+                print("============================")
+                print(title)
+                print(imgurl)
+                print(alt)
+#            print(alt)
+                print("============================")
+                yield {
+                    'title': title,
+                    'imgurl': imgurl,
+                    'alt': alt,
+                }
+
+
+#            yield {
+#                'title': title,
+#                'imgurl': strip.xpath('.´/div[@id="comic"]/img/@src').extract_first(),
+#                'alt': strip.xpath('./div[@id="comic"]/img/@title').extract_first(),
+#            }
+

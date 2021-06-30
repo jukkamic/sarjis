b859bf1ea772d109008a4cba061131daf6586bcf
jukkamic <jukkamic@gmail.com>
2021-06-30 11:15:41 +0300

    recursive crawl for xkcd

--- sarjis/spiders/xkcd.py
+++ sarjis/spiders/xkcd.py
@@ -1,18 +1,15 @@
 import scrapy
 
-
 class XkcdSpider(scrapy.Spider):
     name = 'xkcd'
- #   allowed_domains = ['xkcd.com']
- #   start_urls = ['http://xkcd.com/']
-#    allowed_domains = ['*']
+#    allowed_domains = ['xkcd.com']
+#    start_urls = ['http://xkcd.com/']
     start_urls = ["http://54.217.147.107/xkcd/"]
+
+    count:int = 1
 
     def parse(self, response):
         for comic in response.xpath('//div[@id="middleContainer"]'):
-            #            comic = response.xpath('//div[@id="middleContainer"]')
-
-            #            print("COMIC === " + comic)
             title = comic.xpath('//div[@id="ctitle"]/text()').extract_first()
             imgurl = "https:" + comic.xpath('//div[@id="comic"]/img/@src').extract_first()
             alt = comic.xpath('//div[@id="comic"]/img/@title').extract_first()
@@ -22,10 +19,12 @@ class XkcdSpider(scrapy.Spider):
                 'imgurl': imgurl,
                 'alt': alt,
             }
+        next_page = response.xpath('//a[@rel="prev"]/@href').extract_first()
+        if next_page is not None and self.count < 5:
+            self.count = self.count + 1
+            # remove count limit
+            print("count: ", self.count)
+#            yield scrapy.Request(response.urljoin(next_page))
+            yield scrapy.Request(response.urljoin("/xkcd" + next_page))
 
 
-#            yield {
-#                'title': title,
-#                'imgurl': strip.xpath('.´/div[@id="comic"]/img/@src').extract_first(),
-#                'alt': strip.xpath('./div[@id="comic"]/img/@title').extract_first(),
-#            }

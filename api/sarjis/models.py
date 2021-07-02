8728a0d0f65878b2ab645c8c79f46263c9f8d7ae
jukkamic <jukkamic@gmail.com>
2021-07-02 18:30:08 +0300

    Comic model

--- api/sarjis/models.py
+++ api/sarjis/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Comic(models.Model):
     name = models.CharField(max_length=100)
-    date_crawl = models.DateField('date crawled')
-    date_publish = models.DateField('date published')
-    number = models.IntegerField()
-    title = models.CharField(max_length=100)
-    alt = models.TextField()
-    img_url = models.URLField()
+    date_crawl = models.DateField('date crawled', blank=True, null=True)
+    date_publish = models.DateField('date published', blank=True, null=True)
+    number = models.IntegerField(null=True)
+    title = models.CharField(max_length=100, null=True)
+    alt = models.TextField(null=True)
+    img_url = models.TextField()
 
     def __str__(self):
-        return (self.name, ": ", self.title, "\n", self.img_url)
+        return f'\n{self.name}: {self.title}\n{self.date_crawl}\n{self.img_url}\n'

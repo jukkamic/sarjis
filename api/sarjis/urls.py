a4668c1b31aca37294816b886ff11e6c57be0034
jukkamic <jukkamic@gmail.com>
2021-07-03 01:27:41 +0300

    api for get, add

--- api/sarjis/urls.py
+++ api/sarjis/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = {
-    path('', views.index, name='index'),
-    path('<int:comic_id>/', views.get, name='get')
+#    url(r'^sarjis/$', views.comicApi),
+#    url(r'^sarjis/([0-9]+)$', views.comicApi),
+
+#    path('', views.index, name='index'),
+    path('<int:comic_id>/', views.comicApi),
+    path('', views.comicApi)
 }
--- api/sarjis/views.py
+++ api/sarjis/views.py
@@ -7,13 +7,20 @@ from .models import Comic
 from .serializers import ComicSerializer
 
 
-def index(request):
-    latest_comic_list = Comic.objects.order_by('-date_publish')[:5]
-    output = ', '.join([c.title for c in latest_comic_list])
-    return HttpResponse(output)
-
 @csrf_exempt
-def get(request, comic_id):
-    comic = Comic.objects.get(id=comic_id)
-    comic_serializer = ComicSerializer(comic, many=False)
-    return JsonResponse(comic_serializer.data, safe=False)
+def comicApi(request, comic_id=0):
+    if request.method=='GET':
+        comic = Comic.objects.get(id=comic_id)
+        comic_serializer = ComicSerializer(comic, many=False)
+        return JsonResponse(comic_serializer.data, safe=False)
+    elif request.method=='POST':
+        comic_data = JSONParser().parse(request)
+        comic_serializer = ComicSerializer(data=comic_data)
+        if comic_serializer.is_valid():
+            comic_serializer.save()
+            return JsonResponse("Added successfully!", safe=False)
+        return JsonResponse("Failed to add.", safe=False)
+
+#    latest_comic_list = Comic.objects.order_by('-date_publish')[:5]
+#    output = ', '.join([c.title for c in latest_comic_list])
+#    return HttpResponse(output)

6dd097b105517dfda9873e3b01e6b48b8df9fe32
Jukka <jukkamic@gmail.com>
2021-07-10 10:02:42 +0000

    prod env point to aws sarjis2

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  img_url: "http://localhost:8000/images",
-  api_url: "http://localhost:8000/sarjis/",
+  img_url: "http://54.195.99.44:8000/images",
+  api_url: "http://54.195.99.44:8000/sarjis/",
 };

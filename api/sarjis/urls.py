b8ccda6c50093678206d3033fefee9fc101e3625
jukkamic <jukkamic@gmail.com>
2021-07-13 21:01:30 +0300

    fix to how previous comic links are handled

--- api/sarjis/urls.py
+++ api/sarjis/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from django.views.decorators.cache import cache_page
 from . import views
 
-timeout = 1
+timeout = 60*60
 
 urlpatterns = {
     path('<str:name>/<int:id>/', cache_page(timeout)(views.getComic)),
--- api/sarjis/views.py
+++ api/sarjis/views.py
@@ -36,8 +36,9 @@ def getLatest(request, name:str):
             comic = comic_serializer.save()            
             return fetch_prev_and_update_links(name, comic)
         else:
+            print("Invalid serializer for latest comic: ", comic_serializer.errors)
             return JsonResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data = {
-                 "message": print("Invalid serializer for latest comic: ", comic_serializer.errors)})
+                 "message": [{"Invalid serializer for latest comic: ", comic_serializer.errors}]})
 
 @csrf_exempt
 def getAllLatest(request):
@@ -62,21 +63,34 @@ def getAllLatest(request):
                          ], safe=False)
 
 def fetch_prev_and_update_links(name, comic):
-    # fetch prev, add current id as next, update prev_id for current
-    prev_comic_json = parse(name, comic.prev_link)
-    addComicMeta(name, prev_comic_json)
-    prev_comic_json['next_id'] = comic.id
+    if not comic.prev_link:
+        print("Comic ", comic.name, ",\n", comic.perm_link, ",\nhas no predecessor.")
+        return JsonResponse(ComicSerializer(comic, many=False).data, safe=False)
+    else:
+        # fetch prev, add current id as next, update prev_id for current
+        prev_comic:Comic=None
+        try:
+            prev_comic = Comic.objects.get(perm_link = comic.prev_link)
+            prev_comic.next_id = comic.id
+            prev_comic.save()
+        except Comic.DoesNotExist:
+            prev_comic_json = parse(name, comic.prev_link)
+            addComicMeta(name, prev_comic_json)
+            prev_comic_json['next_id'] = comic.id
 
-    prev_comic_serializer = ComicSerializer(data = prev_comic_json, many=False)
-    if prev_comic_serializer.is_valid():
-        prev_comic = prev_comic_serializer.save()
+            prev_comic_serializer = ComicSerializer(data = prev_comic_json, many=False)
+            if prev_comic_serializer.is_valid():
+                prev_comic = prev_comic_serializer.save()
+            else:
+                print("Invalid serializer for previous comic: ", prev_comic_serializer.errors)
+                return JsonResponse(
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                    data = {"message": "Serializer error handling previous comic",
+                        "error": prev_comic_serializer.errors}
+                )
         comic.prev_id = prev_comic.id
         comic.save()
         return JsonResponse(ComicSerializer(comic, many=False).data, safe=False)
-    else:
-#        print("Invalid serializer for previous comic: ", prev_comic_serializer.errors)
-        return JsonResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data = {
-            "message": print("Invalid serializer for previous comic: ", prev_comic_serializer.errors)})
 
 def parse(name, url):
     if name=="fingerpori":

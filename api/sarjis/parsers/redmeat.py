f1f5d07ac47d02d655bc91541bac04c8db6009c5
jukkamic <jukkamic@gmail.com>
2021-07-20 21:59:49 +0300

    remove debug prints

--- api/sarjis/parsers/redmeat.py
+++ api/sarjis/parsers/redmeat.py
@@ -4,20 +4,15 @@ from bs4 import BeautifulSoup
 class RedmeatParser():
     
     def parse(path, title_in_html=""):
-        print("Red Meat, path: ", path)
         if path == "/":
             path = "/max-cannon/FreshMeat"
         page_html = Common.fetchPage("www.redmeat.com", path)
 
-        print("page html ", page_html[0:300])
         soup = BeautifulSoup(page_html, features="lxml")
 
         if path == "/max-cannon/FreshMeat":
-            print("looking for front page")
             link_attr = soup.find("link", attrs={"rel": "canonical"})
-            print("link_attr:", link_attr)
             perm_link = link_attr["href"].split("redmeat.com")[-1]
-            print("perm link: ", perm_link)
             return RedmeatParser.parse(perm_link)
 
         perm_link = path

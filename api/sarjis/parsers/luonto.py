e78698acd2f8efb535330b7832404f5eec5e784c
Jukka Michelsson <jukkamic@gmail.com>
2022-09-28 07:14:23 +0300

    fix kamala luonto

--- api/sarjis/parsers/luonto.py
+++ api/sarjis/parsers/luonto.py
@@ -1,11 +1,14 @@
 from .common import Common
 from bs4 import BeautifulSoup
+import logging
+
+log = logging.getLogger('sarjis')
 
 class LuontoParser:
 
     def parse(path, title_in_html=""):
         # Always fetch page from base path "/aihe/Kamala_luonto".
-        # If given path is "/" look for the first div class="card card--scrollable-image".
+        # If given path is "/" look for the first article class="card card--scrollable-image".
         # If another path is given search for it in the list at "/".
         # In both cases find prev link info from the next card.
         # TODO: A plan for traversing comics back further than page 1.
@@ -16,14 +19,22 @@ class LuontoParser:
         grid_column = soup.find("h1", text=title_in_html, attrs={'class': 'diks-title'}).find_parent("div")
 
         if path == "/":
-            first_card = grid_column.find('div', attrs={'class': 'diks-card diks-card--scrollable-image'})
+            first_card = grid_column.find('article', attrs={'class': 'diks-card diks-card--scrollable-image'})
             perm_link = first_card.find('a', attrs={'class': 'diks-card__link'})['href']
             return LuontoParser.parse(perm_link, title_in_html)
 
         perm_link = path
-        this_card = grid_column.find('a', href=path, attrs={'class': 'diks-card__link'}).find_parent('div', attrs={'class': 'diks-card diks-card--scrollable-image'})
-        older_card = this_card.find_next_sibling('div', attrs={'class': 'diks-card diks-card--scrollable-image'})
-        newer_card = this_card.find_previous_sibling('div', attrs={'class': 'diks-card diks-card--scrollable-image'})
+        this_card = grid_column.find('a', href=path, attrs={'class': 'diks-card__link'}).find_parent('article', attrs={'class': 'diks-card diks-card--scrollable-image'})
+
+        older_card = None
+        newer_card = None
+        try:
+            older_card = this_card.find_next_sibling('article', attrs={'class': 'diks-card diks-card--scrollable-image'})
+            newer_card = this_card.find_previous_sibling('article', attrs={'class': 'diks-card diks-card--scrollable-image'})
+        except AttributeError as ae:
+            log.error("Finding sibling for card: ", this_card)
+            log.error(ae)
+
         next_link = None
         prev_link = None
 
